c42562ec6d3e5fe5cd736fb074890bd0a7cc4981
Christopher Bartling <chris.bartling@gmail.com>
2020-09-06 10:52:13 -0500

    Sequelize up and running

--- features/step_definitions/GlobalSteps.js
+++ features/step_definitions/GlobalSteps.js
@@ -1,9 +1,11 @@
 import {After, Before} from "cucumber";
 
 Before(async function(testCase) {
+    await this.openDatabaseConnection();
     return await this.openBrowserPage();
 });
 
 After(async function() {
+    await this.closeDatabaseConnection();
     return await this.closeBrowser();
 });
--- features/support/world.js
+++ features/support/world.js
@@ -1,7 +1,12 @@
 import * as dotenvConfig from "dotenv/config";
-import { setWorldConstructor, setDefaultTimeout } from "cucumber";
+import {setWorldConstructor, setDefaultTimeout} from "cucumber";
 import puppeteer from "puppeteer";
+import Sequelize from "sequelize";
 
+const DATABASE = process.env.DATABASE;
+const DB_USERNAME = process.env.DB_USERNAME;
+const DB_PASSWORD = process.env.DB_PASSWORD;
+const DB_HOSTNAME = process.env.DB_HOSTNAME;
 const WEBAPP_URL = process.env.WEBAPP_URL;
 const HEADLESS = process.env.HEADLESS !== 'false';
 
@@ -9,9 +14,37 @@ setDefaultTimeout(30 * 1000);
 
 class GummiBearsWorld {
 
+  async openDatabaseConnection() {
+    this.sequelize = new Sequelize(`postgres://${DB_USERNAME}:${DB_PASSWORD}@${DB_HOSTNAME}:5432/${DATABASE}`, {
+      dialect: 'postgres',
+      dialectOptions: {
+        ssl: {
+          require: true,
+          rejectUnauthorized: false
+        }
+      },
+      logging: false,
+    });
+    try {
+      await this.sequelize.authenticate();
+    } catch (error) {
+      console.error('Unable to connect to the database:', error);
+    }
+  }
+
+  async closeDatabaseConnection() {
+    if (this.sequelize) {
+      try {
+        await this.sequelize.close();
+      } catch (error) {
+        console.error('Unable to disconnect from the database:', error);
+      }
+    }
+  }
+
   async openBrowserPage() {
     this.url = WEBAPP_URL;
-    this.browser = await puppeteer.launch({ headless: HEADLESS });
+    this.browser = await puppeteer.launch({headless: HEADLESS});
     this.page = await this.browser.newPage();
   }
 
